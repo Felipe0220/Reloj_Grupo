5cf69416c3dfb02d8a7281a863a0667ec746597d
Felipe0220 <lusalaspe@est.utn.ac.cr>
2025-07-24 15:53:48 -0600

    Cambios
    No la habia guardado

--- src/ClaseBaseReloj/Tiempo.java
+++ src/ClaseBaseReloj/Tiempo.java
@@ -8,6 +8,45 @@ package ClaseBaseReloj;
  *
  * @author Luisf
  */
-public class Tiempo {
+public abstract class Tiempo {
+    private int hora;
+    private int minuto;
+    private int segundo;
+
+    public Tiempo() {
+        this.hora = 0;
+        this.minuto = 0;
+        this.segundo = 0;
+    }
+    public void avanzar(){
+        segundo++;
+        if (segundo == 60){
+            segundo = 0;
+            minuto++;
+            if (minuto == 60){
+                minuto = 0;
+                hora++;
+                if (hora == 24){
+                    hora = 0;
+                }
+            }
+        }
+    }
     
+    public String toString(){
+        String h, m, s;
+        h = String.valueOf(hora);
+        if (hora < 10){
+            h = "0".concat(h);
+        }
+        m = String.valueOf(minuto);
+        if (minuto < 10){
+            h = "0".concat(m);
+        }
+        s = String.valueOf(segundo);
+        if (segundo < 10){
+            h = "0".concat(s);
+        }
+        return h+":"+m+":"+s;
+    }
 }
