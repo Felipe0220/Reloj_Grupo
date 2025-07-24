7354e37a6018a7f8f41c54fe088b0e1c736af0ed
Kevinq <kevinquiros.m@gmail.com>
2025-07-24 16:07:47 -0600

    Clase configurable
    Se crea la clase

---
+++ src/Reloj/Configurable.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Reloj;
+
+/**
+ *
+ * @author kevin
+ */
+public interface Configurable {
+    public void configurar (int h,int m,int s);
+    
+    
+}
